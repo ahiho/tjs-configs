5a5b938c5741b15ab79c24f8d09b5d3549e36007
cuongnd <cuongnd@ahiho.com>
2024-02-05 15:43:09 +0700

    fix: semantic release

--- packages/eslint-config-ecmascript/release.config.js
+++ packages/eslint-config-ecmascript/release.config.js
@@ -1,34 +1,36 @@
-const name = '@ahiho/eslint-config-ecmascript';
+const name = "@ahiho/eslint-config-ecmascript";
 const srcRoot = `packages/eslint-config-ecmascript`;
 
 module.exports = {
-  extends: 'release.config.base.js',
-  branches: ['main'],
+  extends: "release.config.base.js",
+  branches: ["main"],
   pkgRoot: `dist/${srcRoot}`,
   tagFormat: `${name}@\${version}`,
   commitPaths: [`${srcRoot}/*`],
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
     [
-      '@semantic-release/changelog',
+      "@semantic-release/changelog",
       {
         changelogFile: `${srcRoot}/CHANGELOG.md`,
       },
     ],
     [
-      '@semantic-release/npm',
+      "@semantic-release/npm",
       {
         npmPublish: true,
         pkgRoot: srcRoot,
       },
     ],
-    '@semantic-release/github',
+    "@semantic-release/github",
     [
-      '@semantic-release/git',
+      "@semantic-release/git",
       {
         assets: [`${srcRoot}/package.json`, `${srcRoot}/CHANGELOG.md`],
-        message: `chore(release): ${name} ` + '${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        message:
+          `chore(release): ${name} ` +
+          "${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
   ],
